e89d699260b24441c07b24d9e22a2a8dd2d367a7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 13:25:42 +0300

    Initial Commit

---
+++ SharingAssetsWithAnAppClip/SharingAssetsWithAnAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharingAssetsWithAnAppClipApp.swift
+//  SharingAssetsWithAnAppClip
+//
+//  Created by ramil on 05.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct SharingAssetsWithAnAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
